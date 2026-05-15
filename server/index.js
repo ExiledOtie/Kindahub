ddb61a45d0feb90384819d0964ef74c817a73cbe
ExiledOtie <otieno96jose@gmail.com>
2026-05-15 11:57:08 +0300

    DbCon

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+// DATABASE CONNECTION
+require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Kindahub API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ server/seeders/superAdminSeeder.js
@@ -0,0 +1,52 @@
+const bcrypt = require("bcryptjs");
+const pool = require("../config/db");
+
+const seedSuperAdmin = async () => {
+  try {
+
+    const existingAdmin = await pool.query(
+      `
+      SELECT * FROM users
+      WHERE is_super_admin = true
+      `
+    );
+
+    if (existingAdmin.rows.length > 0) {
+      console.log("Super admin already exists");
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(
+      "Admin@123",
+      10
+    );
+
+    await pool.query(
+      `
+      INSERT INTO users
+      (
+        fullname,
+        email,
+        password,
+        role,
+        is_super_admin
+      )
+      VALUES ($1, $2, $3, $4, $5)
+      `,
+      [
+        "Super Admin",
+        "admin@kindahub.com",
+        hashedPassword,
+        "super_admin",
+        true,
+      ]
+    );
+
+    console.log("Super admin created");
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = seedSuperAdmin;
